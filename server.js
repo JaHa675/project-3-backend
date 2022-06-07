12109cce801fbb4a27a4e0f127aa1330d7387d2d
Lucas Roman <lucas.e.roman@gmail.com>
2022-06-07 14:08:45 -0700

    changed domain to battle-trail-backend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 //PROD MODE
 app.use(cors({
-    origin:"https://obscure-waters-79777.herokuapp.com"
+    origin:"https://battle-trail.herokuapp.com/"
 }));
 
 const PORT = process.env.PORT || 3001;
